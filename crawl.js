ac26475d71c41a201c374f48513f7f4a46bbc990
nebiyu <nebiyuf@gmail.com>
2023-03-07 14:28:30 +0300

    crawal web from provided url and response all url selected

--- crawl.js
+++ crawl.js
@@ -1,18 +1,33 @@
 const jsdom = require("jsdom");
 const axios = require("axios");
 const { JSDOM } = jsdom;
-const crawlCurrentPage = async (currentURL) => {
-  console.log(currentURL);
+const crawlCurrentPage = async (baseUrl, currentURL, pages) => {
+  const baseURLObj = new URL(baseUrl);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname != currentURLObj.hostname) {
+    return pages;
+  }
+  const normalizedUrl = normalizeUrl(currentURL);
+  if (pages[normalizedUrl] > 0) {
+    pages[normalizedUrl]++;
+    return pages;
+  }
+  pages[normalizedUrl] = 1;
+  console.log("actively crawling : ".green, currentURL);
   try {
     const response = await axios.get(currentURL);
-    if (response.headers["Cotent-Type"].includes("text/html")) {
-      console.log(getURLsFromHtml(response.data, currentURL));
+    if (!response.headers["content-type"].includes("text/html")) {
+      return;
     }
-    return;
+    const nextUrls = getURLsFromHtml(response.data, baseUrl);
+    for (const nextUrl of nextUrls) {
+      pages = await crawlCurrentPage(baseUrl, nextUrl, pages);
+    }
   } catch (err) {
-    console.log(err);
     console.log(`Error Occured : ${err.message}`);
   }
+
+  return pages;
 };
 const getURLsFromHtml = function (htmlBody, baseURL) {
   const urls = [];
@@ -28,12 +43,16 @@ const getURLsFromHtml = function (htmlBody, baseURL) {
 };
 
 const normalizeUrl = function (urlString) {
-  const url = new URL(urlString);
-  let urlHostName = url.hostname + url.pathname;
-  if (urlHostName.length > 0 && urlHostName.slice(-1) == "/") {
-    urlHostName = urlHostName.slice(0, urlHostName.length - 1);
+  try {
+    const url = new URL(urlString);
+    let urlHostName = url.hostname + url.pathname;
+    if (urlHostName.length > 0 && urlHostName.slice(-1) == "/") {
+      urlHostName = urlHostName.slice(0, urlHostName.length - 1);
+    }
+    return urlHostName;
+  } catch (err) {
+    console.log("----", err);
   }
-  return urlHostName;
 };
 module.exports = {
   normalizeUrl,
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require("colors");
 const { crawlCurrentPage } = require("./crawl");
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("Please Provide argument".red.underline);
     process.exit(1);
   }
   console.log("crawler starting....".green);
   const baseUrl = process.argv[2];
-  crawlCurrentPage(baseUrl);
+  const pages = crawlCurrentPage(baseUrl, baseUrl, {});
+  console.log(await pages);
 }
 
 main();
